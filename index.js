6f407cc6f6f3fd266ce744191b89f1a124f8694b
Christopher Kang <christopherjkang24@gmail.com>
2021-01-15 01:11:27 +0000

    final working callback hell to find ISS times

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-const { fetchMyIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+// fetchCoordsByIP('99.225.249.188', (error, geoLocation) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned Geolocation:", geoLocation);
+// });
+
+// fetchISSFlyOverTimes(({ latitude: 43.5181, longitude: -80.1873 }), (error, coordinates) => {
+
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned Flyover Times:", coordinates);
+// });
+
+
+
+// Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
+
+const timeConverter = function(pastTimes) {
+
+  for (let time of pastTimes) {
+    const convertedTime = new Date(0);
+
+    convertedTime.setUTCSeconds(time.risetime);
+
+    const duration = time.duration;
+
+
+    console.log(`Next pass at ${convertedTime} for ${duration} seconds!`);
   }
 
-  console.log('It worked! Returned IP:' , ip);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+
+  timeConverter(passTimes);
 });
